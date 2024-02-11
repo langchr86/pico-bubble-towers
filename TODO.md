1f0bca235d205a0fae4494bdb695021faf5de4e7
Christian Lang <lang.chr86@gmail.com>
2024-02-11 20:16:50 +0100

    working astar path finding with little editor test program

---
+++ TODO.md
@@ -0,0 +1,13 @@
+Title: Bubble Towers?
+
+Tutorial
+Title Screen
+Unit Testing -> CI
+Release process -> CI
+Repo
+Licence
+reference of used libraries
+* A star:
+* static code analysis https://github.com/mpeterv/luacheck
+Reference original game: https://games.kidzsearch.com/computer/title/bubble-tanks-tower-defense-41149
+static code analysis
--- pico-proto.lua
+++ pico-proto.lua
@@ -1,21 +1,129 @@
-b1 = {}
-b1.x = 0
-b1.y = 60
-
 function _init()
   cls()
 end
 
+cursor = {x=0, y=0}
+field_width = 8
+field_height = 8
+map_width = 16
+map_height = 16
+
+min_x = 0
+min_y = 0
+max_x = map_width - 1
+max_y = map_height - 1
+
+field = {}
+start = {x = 0, y = 7}
+goal = {x = 15, y = 8}
+object_map = {}
+path = false
+
+btn_up = false
+
 function _update()
-b1.x = b1.x + 1
-    if b1.x > 127 then
-    b1.x = 0
+  if btnp(⬆️) then
+    cursor.y -= 1
+    if cursor.y < min_y then
+      cursor.y = min_y
+    end
   end
+
+  if btnp(⬇️) then
+    cursor.y += 1
+    if cursor.y > max_y then
+      cursor.y = max_y
+    end
+  end
+
+  if btnp(⬅️) then
+    cursor.x -= 1
+    if cursor.x < min_x then
+      cursor.x = min_x
+    end
+  end
+
+  if btnp(➡️) then
+    cursor.x += 1
+    if cursor.x > max_x then
+      cursor.x = max_x
+    end
+  end
+
+  if btnp(🅾️) then
+    current = { x = cursor.x, y = cursor.y }
+
+    removed = false
+    for value in all(object_map) do
+      if value.x == current.x and value.y == current.y then
+        del(object_map, value)
+        removed = true
+        break
+      end
+    end
+
+    if removed == false then
+      add(object_map, current)
+    end
+  end
+
+  if btnp(❎) then
+    local function is_coord_reachable(x, y)
+      -- should return true if the position is open to walk
+      for _, value in pairs(object_map) do
+        if value.x == x and value.y == y then
+          return false
+        end
+      end
+      return true
+    end
+
+    useCache = false
+    excludeDiagonalMoving = true
+    path = module:find(map_width - 1, map_height - 1, start, goal, is_coord_reachable, useCache, excludeDiagonalMoving)
+  end
+
+  --if path == false then
+  --  print ("no path found")
+  --else
+  --  print ("path found")
+  --  for i=1,#path do
+  --    print (path[i].x)
+  --    print (path[i].y)
+  --  end
+  --end
+
 end
 
 function _draw()
   cls()
-  map()
+
+  --for x=0,width do
+  --  for y=0,height do
+  --    local cx = x * 8
+  --    local cy = y * 8
+  --    spr(2, x, y)
+  --  end
+  --end
+
+  for value in all(object_map) do
+    spr(1, value.x * field_width, value.y * field_height)
+  end
+
+  spr(3, start.x * field_width, start.y * field_height)
+  spr(4, goal.x * field_width, goal.y * field_height)
+  spr(6, cursor.x * field_width, cursor.y * field_height)
+
+  if path == false then
+    spr(7, goal.x * field_width, goal.y * field_height)
+  else
+    for i = 1, #path do
+      local x = path[i].x * field_width
+      local y = path[i].y * field_height
+      spr(5, x, y)
+    end
+  end
+  --map()
   --pset(b1.x, b1.y, 12)
-  spr(2, b1.x, b1.y)
+  --spr(2, b1.x, b1.y)
 end

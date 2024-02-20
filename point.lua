d3778bad18537f73ba5d9ddc776ceb5b870f2552
Christian Lang <lang.chr86@gmail.com>
2024-02-20 20:11:52 +0100

    fix code style to use 2 space indentation

--- point.lua
+++ point.lua
@@ -1,15 +1,15 @@
 -- Copyright 2024 by Christian Lang is licensed under CC BY-NC-SA 4.0
 
 Point = {
-    x=0,
-    y=0,
+  x=0,
+  y=0,
 }
 Point.__index = Point
 
 function Point:New(init_x, init_y)
-    o = {
-        x=init_x,
-        y=init_y,
-    }
-    return setmetatable(o, self)
+  o = {
+    x=init_x,
+    y=init_y,
+  }
+  return setmetatable(o, self)
 end

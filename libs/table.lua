c4f5bb1ecb18d29749aa6749703f88ea7c5e0c7c
Christian Lang <lang.chr86@gmail.com>
2024-05-20 16:16:15 +0200

    remove not used qsort

--- libs/table.lua
+++ libs/table.lua
@@ -13,39 +13,3 @@ function table.isort(arr, comp)
       end
   end
 end
-
-function table.qsort(a,c,l,r)
-  c,l,r=c or function(a,b) return a<b end,l or 1,r or #a
-  if l<r then
-    if c(a[r],a[l]) then
-      a[l],a[r]=a[r],a[l]
-    end
-
-    local lp,k,rp,p,q=l+1,l+1,r-1,a[l],a[r]
-
-    while k<=rp do
-      local swaplp=c(a[k],p)
-      if swaplp or c(a[k],q) then
-      else
-        while c(q,a[rp]) and k<rp do
-          rp-=1
-        end
-        a[k],a[rp],swaplp=a[rp],a[k],c(a[rp],p)
-        rp-=1
-      end
-      if swaplp then
-        a[k],a[lp]=a[lp],a[k]
-        lp+=1
-      end
-      k+=1
-    end
-    lp-=1
-    rp+=1
-
-    a[l],a[lp]=a[lp],a[l]
-    a[r],a[rp]=a[rp],a[r]
-    qsort(a,c,l,lp-1       )
-    qsort(a,c,  lp+1,rp-1  )
-    qsort(a,c,       rp+1,r)
-  end
-end

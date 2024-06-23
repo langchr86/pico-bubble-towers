59641596bdbebb6629552d7034d6a71e55dcd0e2
Christian Lang <lang.chr86@gmail.com>
2024-06-23 15:05:42 +0200

    remove procedural wave creation

--- src/difficulty_selection.lua
+++ src/difficulty_selection.lua
@@ -2,9 +2,8 @@
 
 ---@class DifficultySelection
 ---@field mode number
----@field procedural boolean
+---@field manual boolean
 ---@field level number
----@field digit number
 ---@field next_session any
 DifficultySelection = {}
 DifficultySelection.__index = DifficultySelection
@@ -13,63 +12,30 @@ DifficultySelection.__index = DifficultySelection
 function DifficultySelection:New()
   local o = {
     mode = 0,
-    procedural = false,
+    manual = false,
     level = 0,
-    digit = 0,
   }
   return setmetatable(o, self)
 end
 
 function DifficultySelection:Up()
   if self.mode == 0 then
-    self.procedural = not self.procedural
+    self.manual = not self.manual
 
   elseif self.mode == 1 then
-    if self.procedural then
-      local diff = 10 ^ self.digit
-      local limit = diff * 10
-      if self.level % limit < limit - diff then
-        self.level = self.level + diff
-      end
-    else
-      if self.level > 0 then
-        self.level = self.level - 1
-      end
+    if self.level > 0 then
+      self.level = self.level - 1
     end
   end
 end
 
 function DifficultySelection:Down()
   if self.mode == 0 then
-    self.procedural = not self.procedural
+    self.manual = not self.manual
 
   elseif self.mode == 1 then
-    if self.procedural then
-      local diff = 10 ^ self.digit
-      local limit = diff * 10
-      if self.level % limit >= diff then
-        self.level = self.level - diff
-      end
-    else
-      if self.level < 2 then
-        self.level = self.level + 1
-      end
-    end
-  end
-end
-
-function DifficultySelection:Left()
-  if self.mode == 1 then
-    if self.procedural and self.digit < 3 then
-      self.digit = self.digit + 1
-    end
-  end
-end
-
-function DifficultySelection:Right()
-  if self.mode == 1 then
-    if self.procedural and self.digit > 0 then
-      self.digit = self.digit - 1
+    if self.level < 2 then
+      self.level = self.level + 1
     end
   end
 end
@@ -84,17 +50,16 @@ end
 
 function DifficultySelection:PressX()
   if self.mode == 0 then
-    self.digit = 3
-    self.level = 0
-    self.mode = 1
+    if self.manual then
+      --- TODO self.next_session = Manual:New()
+    else
+      self.level = 0
+      self.mode = 1
+    end
   elseif self.mode == 1 then
     self.mode = 2
   else
-    if self.procedural then
-      self.next_session = GameSession:New(CreateProceduralWaveList(self.level))
-    else
-      self.next_session = GameSession:New(CreatePredefinedWaveList(self.level))
-    end
+    self.next_session = GameSession:New(CreatePredefinedWaveList(self.level))
   end
 end
 
@@ -113,20 +78,16 @@ function DifficultySelection:Draw()
 
   print("mode:", 16, 24, 12)
   print("normal", 56, 24, 12)
-  print("procedural", 56, 36, 12)
+  print("manual", 56, 36, 12)
 
-  if self.procedural then
+  if self.manual then
     rect(54, 34, 96, 42, 12)
   else
     rect(54, 22, 96, 30, 12)
   end
 
   if self.mode > 0 then
-    if self.procedural then
-      self:DrawProcedural()
-    else
-      self:DrawNormal()
-    end
+    self:DrawNormal()
   end
 
   if self.mode > 1 then
@@ -143,23 +104,4 @@ function DifficultySelection:DrawNormal()
 
   local y = 52 + self.level * 8
   rect(54, y, 96, y + 8, 12)
-end
-
-function DifficultySelection:DrawProcedural()
-  print("seed:", 16, 54, 12)
-
-  if self.level < 1000 then
-    print("0", 56, 54, 12)
-  end
-  if self.level < 100 then
-    print("0", 60, 54, 12)
-  end
-  if self.level < 10 then
-    print("0", 64, 54, 12)
-  end
-  PrintRight(self.level, 72, 54, 12)
-
-  local x = 68 - self.digit * 4
-  spr(39, x, 45)
-  spr(55, x, 60)
 end

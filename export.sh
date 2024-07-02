d4c164b7ea30cfc8923ef7f398424e1d6cd75808
Christian Lang <lang.chr86@gmail.com>
2024-07-02 21:27:50 +0200

    use --minify-safe-only for export.sh

--- export.sh
+++ export.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-python ../shrinko8/shrinko8.py pico-proto.p8 releases/pico-proto-xx.p8.png --minify --focus-tokens --count --input-count
+python ../shrinko8/shrinko8.py pico-proto.p8 releases/pico-proto-xx.p8.png --minify-safe-only --focus-tokens --count --input-count

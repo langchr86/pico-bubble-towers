d8c332f0849dc3d53e977f962c6121e64c405903
Christian Lang <lang.chr86@gmail.com>
2024-07-22 20:19:50 +0200

    add note about manual steps in web-export.sh

--- web-export.sh
+++ web-export.sh
@@ -7,3 +7,11 @@ rm -f /x/web/fileshare/pico/*
 pico8.exe releases/bubble-towers-xx.p8.png -export /x/web/fileshare/pico/bubble-towers.html
 
 mv /x/web/fileshare/pico/bubble-towers.html /x/web/fileshare/pico/index.html
+
+
+# manually change the index title
+# Bubble Towers - PICO-8 Cartridge
+
+# manually add the following at the end of the index.html
+# <!-- Add content below the cart here -->
+# <p# >BBS: <a href="https://www.lexaloffle.com/bbs/?tid=143267">lexaloffle.com/bbs/?tid=143267</a></p>
